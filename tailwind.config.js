faff242c3ab32a32554adffb0d0043d84d130f1b
Zahid Hasan <md.zahid.hasan.topu@gmail.com>
2024-12-16 04:45:24 +0600

    Font problem Fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,10 @@ module.exports = {
         backgroundImage: {
             'hero-pattern': "url('/img/hero-pattern.svg')",
             'footer-texture': "url('/img/footer-texture.png')",
-        }
-        
+        },
     },
+    
+    
   },
   plugins: [],
 }
